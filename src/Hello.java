ab09f73f17afe8f38b341873a918b6310a543907
Michał Ciechanowski <mciechanowski1991gmail.com.noreply.github.com>
2025-04-24 21:58:39 +0200

    08. if-then Statement

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,16 @@
 public class Hello {
     public static void  main(String[] args) {
         System.out.println("Hello, Olichka");
-        System.out.print("Hello World");
+
+//        boolean isAlien = true;
+//        if (isAlien == false)
+//            System.out.println("It is not an alien!");
+//            System.out.println("And I am scared of aliens");
+
+        boolean isAlien = true;
+        if (isAlien == true) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
