fc011a5137a627ded3ad99726c78ca11ceaf330e
Michał Ciechanowski <mciechanowski1991gmail.com.noreply.github.com>
2025-04-25 20:08:38 +0200

    09. Logical AND Operator

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-    public static void  main(String[] args) {
+    public static void main (String[] args) {
         System.out.println("Hello, Olichka");
 
 //        boolean isAlien = true;
@@ -7,10 +7,20 @@ public class Hello {
 //            System.out.println("It is not an alien!");
 //            System.out.println("And I am scared of aliens");
 
-        boolean isAlien = true;
-        if (isAlien == true) {
+        boolean isAlien = false;
+        if (isAlien == false) {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore >= 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
