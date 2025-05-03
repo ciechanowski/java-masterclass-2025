7f02b00e4954ec2019f7f7583d0a554691e0cc95
Michał Ciechanowski <mciechanowski1991@gmail.com>
2025-05-03 12:55:46 +0200

    02. Keywords and Expressions

---
+++ src/KeywordsAndExpressions.java
@@ -0,0 +1,19 @@
+public class KeywordsAndExpressions {
+    public static void main(String[] args) {
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;  // expression
+
+        if (highScore > 25) {
+            highScore = 1000 + highScore;  // add bonus points
+        }
+
+        int health = 100;
+        if ((health < 25) && (highScore > 1000)) {
+            highScore = highScore - 1000;
+        }
+
+        // 6 total health = 100 / health < 25 / highScore > 1000 / (health < 25) && (highScore > 1000) /
+        // highScore - 1000 / highScore = highScore - 1000 - expressions
+    }
+}
